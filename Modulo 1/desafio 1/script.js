458bbaadcb1dd596845bba58344879a75c164482
unknown <tec.diogoguima@gmail.com>
2020-05-16 18:28:29 -0300

    Form com input

--- Modulo 1/desafio 1/script.js
+++ Modulo 1/desafio 1/script.js
@@ -1,10 +1,15 @@
+'use strict'; // O javascript mostra mais erros
+
 window.addEventListener('load', start);
 
-var globalNames = ['um', 'dois', 'três', 'quatro'];
+var globalNames = [];
 var inputName = null;
+var lista = null;
 
 function start(){
   inputName = document.querySelector('#inputName');
+  lista = document.querySelector('#lista');
+
   preventFormSubmit();
   activateInput();
 }
@@ -19,5 +24,49 @@ function preventFormSubmit(){
 }
 
 function activateInput(){
+  function insertName(newName){
+    globalNames.push(newName);
+
+    render();
+    inputName.value = '';
+    inputName.focus();
+  }
+  function handleTyping(event) {
+    if (event.key == 'Enter') {
+      insertName(event.target.value);
+    }    
+  }
+
+  inputName.addEventListener('keyup', handleTyping);
   inputName.focus();
+}
+
+function render() {
+  function createDeleteButton(index){
+    function deleteName(){
+      console.log('deleteName');
+      globalNames.splice(index, 1);
+      render();
+    }
+    
+    var button = document.createElement('button');
+    button.textContent = 'X';
+    button.addEventListener('click', deleteName);
+
+    return button;
+  }
+
+  lista.innerHTML = '';
+  for (let index = 0; index < globalNames.length; index++) {
+    var li = document.createElement('li');
+
+    var button = createDeleteButton(index);
+
+    var span = document.createElement('span');
+    span.textContent = globalNames[index];
+
+    li.appendChild(button);
+    li.appendChild(span);
+    lista.appendChild(li);
+  }
 }
