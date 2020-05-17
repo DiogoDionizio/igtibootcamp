e28c1d5f6361f43a127adc16e6ba0c36eb97f517
unknown <tec.diogoguima@gmail.com>
2020-05-17 12:54:58 -0300

    Json Url requisicao

--- Modulo 1/desafio 1/array.js
+++ Modulo 1/desafio 1/array.js
@@ -2,7 +2,49 @@ window.addEventListener('load', () => {
   doMap();
   doFilter();
   doForEach();
+  doReduce();
+  doFind();
+  doSome();
+  doEvery();
+  doSort();
+
+  // Spread
+  doSpread();
+  console.log(doRest(1, 2));
+  console.log(doRest(1, 2, 1000, 100));
+
+  // Destruring
+  doDestructuring();
+
 });
+
+function doDestructuring() {
+  const first = people.results[0];
+
+  // Repetitivo
+  //const username = first.login.username;
+  //const password = first.login.password;
+
+  // Usando destructuring
+  const {username, password } = first.login;
+
+  console.log(username);
+  console.log(password);
+}
+
+function doRest(...numbers){
+  return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+
+function doSpread(){
+  const marriedMen = people.results.filter(person => person.name.title === 'Mr');
+  const marriedWoman = people.results.filter(person => person.name.title === 'Ms');
+
+  const marriedPeople = [...marriedMen, ...marriedWoman, {msg: 'oi'}];
+
+  console.log('Casados: ');
+  console.log(marriedPeople);
+}
 
 // MAP
 function doMap(){
@@ -17,6 +59,7 @@ function doMap(){
   return nameEmailArray;
 } 
 
+// Filtrar um array
 function doFilter(){
   const olderThan50 = people.results.filter(person => {
     return person.dob.age > 50;
@@ -25,6 +68,7 @@ function doFilter(){
   console.log(olderThan50);
 }
 
+// ForEach
 function doForEach(){
   const mappedPeople = doMap();
 
@@ -33,4 +77,47 @@ function doForEach(){
   });
 
   console.log(mappedPeople);
+}
+
+// Somar as idades
+function doReduce(){
+  const totalAges = people.results.reduce((accumulator, current) => {
+    return accumulator + current.dob.age;
+  }, 0);
+
+  console.log(totalAges);
+}
+
+function doFind(){
+  const found = people.results.find(person => {
+    return person.location.state === 'Minas Gerais';
+  });
+
+  console.log(found);
+}
+
+function doSome(){
+  const found = people.results.some(person => {
+    return person.location.state === 'Amazonas';
+  });
+
+  console.log(found);
+}
+
+function doEvery(){
+  const every = people.results.every(person => {
+    return person.nat === 'BR';
+  });
+
+  console.log(every);
+}
+
+function doSort() {
+  const mappedNames = people.results.map(person => {
+    return person.name.first;
+  })
+  .filter(person => person.startsWith('A'))
+  .sort();
+
+  console.log(mappedNames);
 }
